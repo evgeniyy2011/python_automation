090bbf26712fe2f2c0b1163915c28cb6b22457b1
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-07-26 18:28:55 +0100

    solved task 6.1

--- Homework_06/HW_6.1.py
+++ Homework_06/HW_6.1.py
@@ -1,13 +1,15 @@
 
-new_sent = str(input("Enter any characters - "))
-sentence = ''
-for i in new_sent:
-    if i not in sentence:
-        sentence+=i
-if len(sentence) > 10:
-    print(True)
-else:
-    print(False)
+new_sent = len(set(input("Enter any characters - ")))
+print(new_sent > 10)
+
+# sentence = ""
+# for i in new_sent:
+#     if i not in sentence:
+#         sentence+=i
+# if len(sentence) > 10:
+#     print(True)
+# else:
+#     print(False)
 
 
 

bba136d8c41b7cd53db5a8c4b8cb8f7d9b04d3e0
Evgeniy Poliezhaiev <evgeniyy2011@gmail.com>
2026-07-30 22:20:12 +0100

    task 6 ver.2

--- leason_bonus/Bonus_6.py
+++ leason_bonus/Bonus_6.py
@@ -1,7 +1,7 @@
 # Вирішити задачу 4 без словника за 2 строки:
 # 1 строка це input
 # 2 строка це рішення
-from curses.ascii import isalpha
+
 
 www = "".join((input("Enter words - ")).split())
 print(set([i+" = "+(str(www.count(i))) for i in www if i.isalpha()]))

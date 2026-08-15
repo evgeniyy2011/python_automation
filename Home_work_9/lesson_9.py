10087349b222455f43c20a1b25db9285f23a5d5f
Evgeniy <evgeniyy2011@gmail.com>
2026-08-15 21:29:04 +0100

    i've done HW 9 (#10)
    * i've done HW 9
    
    * fixed -  HW 9

---
+++ Home_work_9/lesson_9.py
@@ -0,0 +1,30 @@
+class Romb:
+    def __init__(self, side:int, angle_a:int):
+        self.side = side
+        self.angle_a = angle_a
+
+    def __setattr__(self, key, value):
+        if key != "angle_a" and key != "side":
+            raise TypeError("You can't enter it - any number")
+
+        if key == "side":
+            if not isinstance(value, int):
+                raise TypeError("Side should be just int.")
+            if value<=0:
+                raise TypeError("side should be positive and greater than 0")
+
+        if key == "angle_a":
+            if not isinstance(value, int):
+                raise TypeError("Angle should be number (int)")
+            if value<=0 or value>=180:
+                raise TypeError("Angle should be between 0-180")
+            self.__dict__["angle_b"] = 180 - value
+
+        self.__dict__[key] = value
+
+    def __str__(self):
+        return f"Romb --->>> sides = {self.side}, Angle A = {self.angle_a}, Angle B = {self.angle_b}"
+
+www = Romb(100, 179)
+# www.angle_b=1
+print(www)
